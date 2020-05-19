53fa58f3fd1afb8f3274fc9c5f0db780ea0992ea
Bartosz Mleczko <adamweb9827@gmail.com>
2020-05-19 18:28:13 +0200

    initial commit

---
+++ backend/PlantExpertSystem/src/main/java/pl/mleczko/PlantExpertSystem/PlantExpertSystemApplication.java
@@ -0,0 +1,13 @@
+package pl.mleczko.PlantExpertSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantExpertSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantExpertSystemApplication.class, args);
+	}
+
+}
